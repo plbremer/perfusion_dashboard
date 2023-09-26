eb55b1c26a3901b4ddd40187e3745d8c67e97e95
Parker Gleason-Bremer <pgleasonbremer@SDI-P73PG9H79D.int.altoslabs.com>
2023-09-26 16:15:50 -0700

    hacky add of url to button 2

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ local_stylesheet = {
     "rel": "stylesheet"
 }
 
-app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP, local_stylesheet ], url_base_pathname='/content/a537f01f-de6f-4093-b216-89fef5cfba1b/')
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP, local_stylesheet ])#, url_base_pathname='/content/a537f01f-de6f-4093-b216-89fef5cfba1b/')
 
 #custom ordering of navbar
 my_page_link_list=[
     dbc.NavLink('Home', href='/',style = {'color': 'white','font-weight':'bold'},className='navlink-parker'),
-    dbc.NavLink('Dataset Manager', href='/dataset-manager',style = {'color': 'white','font-weight':'bold'},className='navlink-parker'),
+    dbc.NavLink('Dataset Manager', href='/content/a537f01f-de6f-4093-b216-89fef5cfba1b/dataset-manager',style = {'color': 'white','font-weight':'bold'},className='navlink-parker'),
     dbc.NavLink('Unit Manager', href='/unit-manager',style = {'color': 'white','font-weight':'bold'},className='navlink-parker'),
     dbc.NavLink('Plotter', href='/plotter',style = {'color': 'white','font-weight':'bold'},className='navlink-parker'),
 ]
