0874fea215f53c44be32199d4034f437588ad2c9
Parker Gleason-Bremer <pgleasonbremer@MacBook-Pro-4.local>
2023-09-28 14:45:46 -0700

    add config

---
+++ config.py
@@ -0,0 +1,17 @@
+DATAFRAME_DICT=dict()
+
+UNIT_DICT={
+    'pO2':'mmHG',
+    'pCO2':'mmHG',
+    'pH':'No Unit',
+    'Temperature':'ºC',
+    'Bile':'mL',
+    'Glucose':'mmol/L',
+    'ArterialFlow':'L/min.',
+    'PortalFlow':'L/min.',
+    'IvcFlow':'L/min.',
+    'ArterialPressure':'mmHG',
+    'IvcPressure':'mmHG',
+    'O2%':'No Unit',
+    'Air%':'No Unit',
+}
