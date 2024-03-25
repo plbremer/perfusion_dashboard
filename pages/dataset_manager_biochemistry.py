2504c8a6466e685385d6210e02a8da127a2042df
Parker Gleason-Bremer <pgleasonbremer@altoslabs.com>
2024-03-25 11:50:56 -0700

    remove exceptions and reduce printing

--- pages/dataset_manager_biochemistry.py
+++ pages/dataset_manager_biochemistry.py
@@ -51,7 +51,7 @@ def coerce_one_sheet(bytes_object,temp_sheet):
     for temp_df_col in temp_df.columns:
         temp_df[temp_df_col]=temp_df[temp_df_col].astype(str).str.extract(pattern, expand=False)
     temp_df=temp_df.reset_index(drop=True)
-    print(temp_df)
+    # print(temp_df)
     # print(temp_df['pH',:])
     temp_df['pH']=temp_df['pH'].where(
         cond=(temp_df['pH'].astype(float)<20),
